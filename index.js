de5e1a09263bb759276d06e14105c7d39643d29c
DESKTOP-CDT1KT6\OAASIS-08 <bruno.duarte@projetooaasis.com>
2023-01-04 14:42:38 -0300

    commit methods gmail

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
-async function googleAuth(token, client_id) {
-    const token = token;
-    const client_id = client_id;
-    const { OAuth2Client } = require('google-auth-library');
-    const client = new OAuth2Client(client_id);
+const { OAuth2Client } = require('google-auth-library');
 
-    if (!token) {
-        return false;
-    }
-    
-    (async () => {
-        try {
-            const ticket = await client.verifyIdToken({
-                idToken: token, 
-                audience: client_id, 
-            });
-            const payload = ticket.getPayload();
+async function googleAuth(token, client_id) {
+    return {
+        async selectAll() {
+            const client = new OAuth2Client(client_id);
+
+            if (!token) {
+                return false;
+            }
             
-            return payload;
-        }catch(err) {
-            return err;
-        }
-    })();
+            try {
+                const ticket = await client.verifyIdToken({
+                    idToken: token, 
+                    audience: client_id, 
+                });
+                const payload = ticket.getPayload();
+        
+                return payload;
+            }catch(err) {
+                return err;
+            }
+        },
+        async selectUser() {
+            const client = new OAuth2Client(client_id);
+            
+            if (!token) {
+                return false;
+            }
+            
+            try {
+                const ticket = await client.verifyIdToken({
+                    idToken: token, 
+                    audience: client_id, 
+                });
+                const payloadName = ticket.getPayload();
+        
+                return payloadName['name'];
+            }catch(err) {
+                return err;
+            }
+        },
+        async selectEmail() {
+            const client = new OAuth2Client(client_id);
+            
+            if (!token) {
+                return false;
+            }
+            
+            try {
+                const ticket = await client.verifyIdToken({
+                    idToken: token, 
+                    audience: client_id, 
+                });
+                const payloadEmail = ticket.getPayload();
+        
+                return payloadEmail['email'];
+            }catch(err) {
+                return err;
+            }
+        },
+        async selectPicture() {
+            const client = new OAuth2Client(client_id);
+            
+            if (!token) {
+                return false;
+            }
+            
+            try {
+                const ticket = await client.verifyIdToken({
+                    idToken: token, 
+                    audience: client_id, 
+                });
+                const payloadPicture = ticket.getPayload();
+        
+                return payloadPicture['picture'];
+            }catch(err) {
+                return err;
+            }
+        },
+    }
 }
 
-module.exports = { googleAuth }
+module.exports = { googleAuth };
